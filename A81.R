3078e9baf5080df3e5593ec0f8f7f73b22bb5f8e
ms24apr@herts.ac.uk <ms24apr@herts.ac.uk>
2025-12-12 14:21:08 +0000

    Add factor levels and numeric conversion for student data

--- A81.R
+++ A81.R
@@ -9,3 +9,10 @@ if (ncol(student) == 1) {
 
 cat("Rows:", nrow(student), "Cols:", ncol(student), "\n")
 print(names(student))
+
+student$sex  <- factor(student$sex, levels = c("F","M"), labels = c("Female","Male"))
+student$Walc <- as.numeric(student$Walc)
+
+print(aggregate(Walc ~ sex, data = student, FUN = mean))
+print(aggregate(Walc ~ sex, data = student, FUN = sd))
+print(table(student$sex))
