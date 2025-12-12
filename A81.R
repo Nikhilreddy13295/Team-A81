097d20aebb19ef2f5079f20df8b1f3661527c444
gb24abn@herts.ac.uk <gb24abn@herts.ac.uk>
2025-12-12 19:56:28 +0530

    Add boxplot for weekend alcohol consumption by gender
    Added a boxplot to visualize weekend alcohol consumption by gender.

--- A81.R
+++ A81.R
@@ -16,3 +16,12 @@ student$Walc <- as.numeric(student$Walc)
 print(aggregate(Walc ~ sex, data = student, FUN = mean))
 print(aggregate(Walc ~ sex, data = student, FUN = sd))
 print(table(student$sex))
+
+ggplot(student, aes(x = sex, y = Walc, fill = sex)) +
+  geom_boxplot() +
+  labs(
+    title = "Weekend Alcohol Consumption (Walc) by Gender",
+    x = "Gender",
+    y = "Weekend Alcohol Consumption (Walc: 1–5)"
+  ) +
+  theme_minimal()
